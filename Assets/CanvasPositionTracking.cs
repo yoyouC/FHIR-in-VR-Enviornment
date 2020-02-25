f3730a804e5c036960df47f5a62cc57b1d8dad8a
Xuyou Cheng <yoyoucheng@outlook.com>
2020-02-25 13:06:46 +0000

    Added Grabbable Canvas and Canvas Blocker

---
+++ Assets/CanvasPositionTracking.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CanvasControl
+{
+    public class CanvasPositionTracking : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform DefaultTransform;
+        void Start()
+        {
+            transform.localPosition = DefaultTransform.localPosition;
+            transform.localRotation = DefaultTransform.localRotation; 
+        }
+
+        // Update is called once per frame
+        /// <summary>
+        /// OnTriggerEnter is called when the Collider other enters the trigger.
+        /// </summary>
+        /// <param name="other">The other Collider involved in this collision.</param>
+        void OnTriggerEnter(Collider other)
+        {
+            if(other.GetComponent<CanvasBlocker>() != null)
+            {
+                transform.localPosition = DefaultTransform.localPosition;
+                transform.localRotation = DefaultTransform.localRotation; 
+            }
+        }
+
+        /// <summary>
+        /// OnTriggerStay is called once per frame for every Collider other
+        /// that is touching the trigger.
+        /// </summary>
+        /// <param name="other">The other Collider involved in this collision.</param>
+        void OnTriggerStay(Collider other)
+        {
+            if(other.GetComponent<CanvasBlocker>() != null)
+            {
+                transform.localPosition = DefaultTransform.localPosition;
+                transform.localRotation = DefaultTransform.localRotation; 
+            }
+        }
+    }
+}
---
+++ Assets/HandTrackingGrabber.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OculusSampleFramework;
+
+public class HandTrackingGrabber : OVRGrabber
+{
+    private Hand hand;
+    private float pinchThreshold = 0.1f;
+    protected override void Start()
+    {
+        base.Start();
+        hand = GetComponent<Hand>();
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+        CheckIndexPinch();
+    }
+
+    void CheckIndexPinch()
+    {
+        float pinchStrength = hand.PinchStrength(OVRPlugin.HandFinger.Index);
+        bool isPinching = pinchStrength > pinchThreshold;
+
+        if(!m_grabbedObj && isPinching && m_grabCandidates.Count > 0)
+        {
+            GrabBegin();
+        }
+        else if(m_grabbedObj && !isPinching)
+        {
+            GrabEnd();
+        }
+    }
+}
