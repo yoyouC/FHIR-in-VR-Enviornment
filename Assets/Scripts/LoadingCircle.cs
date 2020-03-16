f955bd73b5572fdb82a2dc4cb896fe1efe0c74bf
Xuyou Cheng <yoyoucheng@outlook.com>
2020-03-16 19:08:45 +0000

    Room Environment Created

---
+++ Assets/Scripts/LoadingCircle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class LoadingCircle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Image loadingCircle;
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        this.loadingCircle = this.GetComponent<Image>();
+        loadingCircle.fillAmount = 0;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0,0,-0.3f));
+        loadingCircle.fillAmount +=  (1 / (Math.Abs(loadingCircle.fillAmount - 0.5f) + 0.5f)) * 0.005f;
+        if(loadingCircle.fillAmount >= 1) loadingCircle.fillAmount = 0;
+    }
+}
---
+++ Assets/Scripts/LoadingText.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingText : MonoBehaviour
+{
+    
+    private Text loadingText;
+    private bool corotineStarted = false;
+    void Awake()
+    {
+        this.loadingText = transform.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!corotineStarted)
+        {
+            StartCoroutine(changeText());
+        }
+    }
+
+    IEnumerator changeText()
+    {
+        corotineStarted = true;
+        for(int i = 0; i < 4; i++)
+        {
+            yield return new WaitForSeconds(1);
+            loadingText.text += ".";
+        }
+        loadingText.text = "Loading";
+        corotineStarted = false;
+    }
+}
